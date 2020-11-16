6151e01658fd28bf207bce6f33907ad278c059c0
Marcus Kempe <makem86@gmail.com>
2020-11-16 21:41:16 +0100

    Formatting

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: "plopp:mqtt-collection",
-  version: "1.0.5",
-  summary: "IoT for Meteor - send/receive MQTT messages via collections",
-  git: "https://github.com/plopp/meteor-mqtt-collection.git",
-  documentation: "README.md"
+    name: "plopp:mqtt-collection",
+    version: "1.0.5",
+    summary: "IoT for Meteor - send/receive MQTT messages via collections",
+    git: "https://github.com/plopp/meteor-mqtt-collection.git",
+    documentation: "README.md"
 });
 
 Npm.depends({
-  "mqtt": "4.2.5"
+    "mqtt": "4.2.5"
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom("1.0");
-  api.addFiles("lib/mqtt.js", "server");
-  api.addFiles("lib/mqtt_collection.js", "server");
-  api.export("mqtt");
+Package.onUse(function (api) {
+    api.versionsFrom("1.0");
+    api.addFiles("lib/mqtt.js", "server");
+    api.addFiles("lib/mqtt_collection.js", "server");
+    api.export("mqtt");
 });
